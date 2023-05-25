bf1bf90c7e0b85ef5da970fbecc09192a91d054d
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-25 14:03:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+const prom1 = new Promise((resolve) => setTimeout(resolve, 1000, "Hello, world!"));
+
+prom1.then((value) => document.getElementById('output').innerText = value);
